77cb7e85623c98fbee5bc8b6417edece66e2a8f5
Emily Pillmore <emilyp@cakesolutions.net>
2017-12-01 18:03:47 -0500

    updates

--- src/main/scala/net/cakesolutions/interview/examples/MonoidExamples.scala
+++ src/main/scala/net/cakesolutions/interview/examples/MonoidExamples.scala
@@ -4,16 +4,29 @@ import net.cakesolutions.interview.Monoid
 
 object MonoidExamples {
 
+  /**
+    * The simplest example of monoidal structure
+    */
   implicit val additiveMonoid: Monoid[Int] = new Monoid[Int] {
     override def mappend(a1: Int, a2: Int): Int = a1 + a2
     override def mzero: Int = 0
   }
 
-  implicit val multiplicativeMonoid: Monoid[Int] = new Monoid[Int] {
-    override def mappend(a1: Int, a2: Int): Int = a1 * a2
-    override def mzero: Int = 1
-  }
+  /**
+    * Note also, the standard Int forms a monoid under (Int, *, 1).
+    * This example is to avoid a conflict with the previously defined implicit
+    */
+  implicit val pairMultiplicativeMonoid: Monoid[(Int, Int)] =
+    new Monoid[(Int, Int)] {
+      override def mappend(a1: (Int, Int), a2: (Int, Int)): (Int, Int) =
+        (a1._1 * a2._1, a1._2 * a2._2)
+      override def mzero: (Int, Int) = (1, 1)
+    }
 
+  /**
+    * In fact, this is independent of the choice of `A`. Ostensibly, List is a *free* monoid
+    * in a principled language/theory.
+    */
   implicit def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
     override def mappend(a1: List[A], a2: List[A]): List[A] = a1 ::: a2
     override def mzero: List[A] = Nil
